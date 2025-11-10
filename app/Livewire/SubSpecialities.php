22700cae8d3395a2040edf9825dcda5e71985533
Aisha63110 <aisha.tadicha@strathmore.edu>
2025-11-10 22:23:27 +0300

    Lab 7.8.0.2 sub speciality [livewire] --add, edit ,delete

--- app/Livewire/SubSpecialities.php
+++ app/Livewire/SubSpecialities.php
@@ -3,18 +3,28 @@
 namespace App\Livewire;
 
 use App\Models\SubSpeciality;
+use App\Models\Speciality;
 use Livewire\Component;
 use Livewire\WithPagination;
+use App\Rules\AlphaSpaces;
+
 
 class SubSpecialities extends Component
 {
+    
     use WithPagination;
 
     protected $paginationTheme = 'bootstrap';
 
     public $search = null;
     public $orderField = 'name';       // default ordering field
-    public $orderDirection = 'asc';    // default ordering direction
+    public $orderDirection = 'asc';   // default ordering direction
+    public $name;
+    public $speciality;
+    public $id;
+    public $showForm = false;
+    public $isEditing = false;
+   
 
     // Reset pagination when search changes
     public function updatingSearch()
@@ -24,6 +34,7 @@ class SubSpecialities extends Component
 
     public function render()
     {
+        $specialities = Speciality::all();
         $query = SubSpeciality::query();
 
         if (!is_null($this->search)) {
@@ -34,13 +45,13 @@ class SubSpecialities extends Component
             ->orderBy($this->orderField, $this->orderDirection)
             ->paginate(env('PAGINATION_COUNT', 10));
 
-        return view('livewire.sub-specialities', compact('subspecialities'));
+        return view('livewire.sub-specialities', compact('subspecialities', 'specialities'));
     }
     public function orderBy($field)
     {
         // update the order field
         $this->orderField = $field;
-        if ($this->orderDirection=== 'asc') {
+        if ($this->orderDirection === 'asc') {
             // Toggle to descending order
             $this->orderDirection = 'desc';
         } else {
@@ -48,9 +59,69 @@ class SubSpecialities extends Component
             $this->orderDirection = 'asc';
         }
     }
+    public function rules()
+    {
+        return [
+            'name' => ['required','max:30', new AlphaSpaces()],
+            'speciality' => ['required','integer']
+        ];
+    }
     public function clearSearch()
     {
         $this->search = " ";
     }
-}
+    public function add()
+    {
+        //rest the form
+        $this->reset();
+        $this->showForm = true;
+    }
+    public function cancel()
+    {
+        $this->reset();
+        $this->showForm = false;
+    }
+    public function store()
+    {
+        //validation
+        $this->validate();
 
+        //save
+        SubSpeciality::create([
+            'name' => $this->name,
+            'speciality_id' => $this->speciality
+        ]);
+        $this->reset();
+        $this->showForm = false;
+
+        //give a message back to the user
+        session()->flash('success', 'Sub-Speciality added successfully.');
+    }
+    public function edit($id){
+        // selct based on id
+        $subspeciality = SubSpeciality::findOrFail($id);
+        $this->name = $subspeciality->name;
+        $this->id = $subspeciality->id;
+        $this->speciality = $subspeciality->speciality_id;
+        $this->showForm = true;
+        $this->isEditing = true;
+    }
+   public function update($id){
+    $subSpeciality = SubSpeciality::findOrFail($id);
+    $subSpeciality->name = $this->name;
+    $subSpeciality->speciality_id = $this->speciality;
+    $subSpeciality->save();
+
+    // update the state of our variable
+    $this -> reset();
+    $this ->isEditing = false;
+    $this -> showForm = false;
+
+    session()->flash('success', 'Sub-Speciality updated successfully.');
+   } 
+   public function destroy($id){
+    $subSpeciality = SubSpeciality::findOrFail($id);
+    $subSpeciality->delete();
+    session()->flash('success', 'Sub-Speciality deleted successfully.');
+   }
+}
--- resources/views/livewire/sub-specialities.blade.php
+++ resources/views/livewire/sub-specialities.blade.php
@@ -1,97 +1,152 @@
-<div class= "col-md-12">
-    {{-- root element  --} }
+<div class="col-md-12">
+    {{-- root element --}}
 
-    {{-- table --}}
-
-<div class="card">
-        <div class="card-header">
-            <h3 class="card-title">All SubSpecialities</h3>
-            <div class="card-tools">
-                <a href="#" class="btn btn-primary btn-sm">
-                    <i class="bi bi-plus-circle"></i> Add New Sub Speciality
-                </a>
-
-                <form class="d-inline-block me-2">
-                    <div class="input-group input-group-sm">
-                        {{--  show inline error messages --}}
-                        <input wire:model.live.debounce:200ms="search" type="text" name="search"
-                            class="form-control {{ $errors->has('search') ? 'is-invalid' : '' }}"
-                            title="{{ $errors->has('search') ? 'Error: may only contain letters and spaces' : '' }}"
-                            placeholder="Search Specialities"
-                            {{ request()->has('search') && trim(request('search')) !== '' ? 'autofocus' : '' }}>
-                    </div>
-                </form>
-
-                <a href="#" wire:click="clearSearch" title="Reset" class="btn btn-success">
-                    <i class="bi bi-arrow-clockwise"></i>
-                </a>
-
-            </div>
+    {{-- show messages --}}
+    @if (session()->has('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
         </div>
-        <!-- /.card-header -->
+    @endif
 
-        <div class="card-body">
-            <table class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th style="width: 10px">#</th>
-                        <th>
-                            <a href="#" wire:click="orderBy('name')" >
-                                Name
-                            </a>
-                            @if($orderDirection == "asc")
-                                <i class = "bi bi-sort-alpha-up"></i>
-                            @else    
-                                <i class = "bi bi-sort-alpha-down"></i>
-                            @endif
-                        </th>
-                        <th>Speciality</th>
-                        <th>Created At</th>
-                        <th style="width: 200px">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($subspecialities as $subspeciality)
+    @if (session()->has('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
+
+    @if($showForm)
+        {{-- form --}}
+        <div class="card card-primary card-outline mb-4">
+            <div class="card-header">
+                <div class="card-title">{{ $isEditing ? "Edit " : "Add " }} Sub Speciality</div>
+            </div>
+            <form wire:submit.click="{{ $isEditing ? "update($id)" : "store" }}">
+                @csrf
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input required wire:model="name" type="text" name="name"
+                            class="form-control @error('name') is-invalid @enderror">
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="speciality" class="form-label">Speciality</label>
+                        <select name="speciality" class="form-control @error('speciality') is-invalid @enderror"
+                            wire:model="speciality">
+                            <option value=""> -- Please Select -- </option>
+                            @foreach ($specialities as $speciality)
+                                <option value="{{ $speciality->id }}">
+                                    {{ $speciality->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                        @error('speciality')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">
+                        <i class="bi-icons bi-save"></i> {{ $isEditing ? " Save Changes" : "Submit" }}
+                    </button>
+                    <a href="#" wire:click="cancel" class="btn btn-secondary">
+                        <i class="bi bi-arrow-left"></i> Back
+                    </a>
+                </div>
+            </form>
+        </div>
+    @else
+        {{-- table --}}
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">All SubSpecialities</h3>
+                <div class="card-tools">
+                    <a href="#" class="btn btn-primary btn-sm" wire:click="add">
+                        <i class="bi bi-plus-circle"></i> Add New Sub Speciality
+                    </a>
+
+                    <form class="d-inline-block me-2">
+                        <div class="input-group input-group-sm">
+                            {{-- show inline error messages --}}
+                            <input wire:model.live.debounce:200ms="search" type="text" name="search"
+                                class="form-control {{ $errors->has('search') ? 'is-invalid' : '' }}"
+                                title="{{ $errors->has('search') ? 'Error: may only contain letters and spaces' : '' }}"
+                                placeholder="Search Specialities"
+                                {{ request()->has('search') && trim(request('search')) !== '' ? 'autofocus' : '' }}>
+                        </div>
+                    </form>
+
+                    <a href="#" wire:click="clearSearch" title="Reset" class="btn btn-success">
+                        <i class="bi bi-arrow-clockwise"></i>
+                    </a>
+                </div>
+            </div>
+            <!-- /.card-header -->
+
+            <div class="card-body">
+                <table class="table table-bordered table-striped">
+                    <thead>
                         <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $subspeciality->name }}</td>
-                           <td>{{ $subspeciality->speciality->name ?? '—' }}</td>
+                            <th style="width: 10px">#</th>
+                            <th>
+                                <a href="#" wire:click="orderBy('name')">Name</a>
+                                @if($orderDirection == "asc")
+                                    <i class="bi bi-sort-alpha-up"></i>
+                                @else
+                                    <i class="bi bi-sort-alpha-down"></i>
+                                @endif
+                            </th>
+                            <th>Speciality</th>
+                            <th>Created At</th>
+                            <th style="width: 200px">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($subspecialities as $subspeciality)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $subspeciality->name }}</td>
+                                <td>{{ $subspeciality->speciality->name ?? '—' }}</td>
+                                <td>{{ date('M d, Y', strtotime($subspeciality->created_at)) }}</td>
+                                <td>
+                                    <div class="btn-group" role="group">
+                                        <a href="#" class="btn btn-info btn-sm" title="View">
+                                            <i class="bi bi-eye"></i>
+                                        </a>
+                                        <a href="#" wire:click="edit({{ $subspeciality->id }})" class="btn btn-warning btn-sm" title="Edit">
+                                            <i class="bi bi-pencil"></i>
+                                        </a>
 
-                            <td>{{ date('M d, Y', strtotime($subspeciality->created_at)) }}</td>
-                            <td>
-                                <div class="btn-group" role="group">
-                                    <a href="#"
-                                        class="btn btn-info btn-sm" title="View">
-                                        <i class="bi bi-eye"></i>
-                                    </a>
-                                    <a href="#"
-                                        class="btn btn-warning btn-sm" title="Edit">
-                                        <i class="bi bi-pencil"></i>
-                                    </a>
-
-                                    <form action="#" method="POST"
-                                        class="d-inline"
-                                        onsubmit="return confirm('Are you sure you want to delete this Sub Speciality?');">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger btn-sm" title="Delete">
+                                        <button type="button"
+                                            wire:click="destroy({{ $subspeciality->id }})"
+                                            class="btn btn-danger btn-sm"
+                                            title="Delete"
+                                            onclick="return confirm('Are you sure you want to delete this Sub Speciality?');">
                                             <i class="bi bi-trash"></i>
                                         </button>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.card-body -->
 
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-            {{-- Pagination --}}
-            <div class="mt-3">
-                {{ $subspecialities->links() }}
+            <div class="card-footer">
+                {{-- Pagination --}}
+                <div class="mt-3">
+                    {{ $subspecialities->links() }}
+                </div>
             </div>
         </div>
-    </div>  
-</div>    
+    @endif
+</div>

6a463c24343afdb172cdf20feba6ee634fbe51bb
Aisha63110 <aisha.tadicha@strathmore.edu>
2025-11-21 15:54:21 +0300

    Lab 10.1.2 - api token-based authentication

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');// api authentication
 
-// route to practitioner api controller
+
+Route::middleware('auth:sanctum')->group(function () {
+
+   // route to practitioner API controller
 Route:: apiResource('v1/practitioners', App\Http\Controllers\v1\PractitionerApiController::class);
 
+//verifying practitioner
 Route::post('v1/practitioners/verify', [App\Http\Controllers\v1\PractitionerApiController::class, 'verify']);
+
+});
